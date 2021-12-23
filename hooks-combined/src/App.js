9d7552de263a92c660bc0cdc7be1a7d322f13ed2
Prasanna Nimbalkar <prasannanimbalkar@Prasannas-MacBook-Pro.local>
2021-12-23 08:31:45 -0500

    modifying

---
+++ hooks-combined/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+
+export default function App() {
+
+  const [name, setName] = useState('')
+
+  const prevName = useRef('')
+
+  useEffect(() => {
+    prevName.current = name
+    }, [name])
+    
+  return (
+    <>
+      <input value={name} onChange={e => setName(e.target.value)}/>
+      <div>My name is {name} and it used to be {prevName.current}</div>
+    </>)
+}
